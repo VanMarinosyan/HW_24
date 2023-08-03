89aa4eb5148bb51eb6f00055d95b378843c065fc
Van <Vano0492@gmail.com>
2023-08-03 22:56:42 +0400

    hw

--- main.py
+++ main.py
@@ -5,3 +5,27 @@ def generate_christmas_tree(height):
         print(' ' * (height - i - 1) + '*' * (2 * i + 1))
 
 generate_christmas_tree(5)
+
+# hw_3
+
+def climb_stairs(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 2
+
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n]
+
+n = 10
+ways = climb_stairs(n)
+print("Number of distinct ways to climb to the top:", ways)
+
